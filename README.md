dea0e5a6259b3c08cb7d45694dcd1fe4744183d4
MarahIrshaid <marah.irshaid96@gmail.com>
2020-07-07 18:34:15 +0300

    lab03 and fixed lab02

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 201d7-class02-lab02
 Hello , Welcome to class02 lab02 Solution :
+also lab03 thank you!
--- js/scripts.js
+++ js/scripts.js
@@ -1,102 +1,187 @@
 'use strict'
 var userName = prompt("Hi, What is your name?");
-alert("Hello!    " + userName + " \b\b\b  Welcome to my lifeStory!   ");
+alert("Hello!    " + userName + "       Welcome to my lifeStory!   ");
 
 
-var myBirthCity;
 
-while (myBirthCity !== "yes") {
-    myBirthCity = prompt("Do you think I was born in Zarqa?");
-    myBirthCity.toLowerCase();
 
-    if (myBirthCity === "yes") {
-        alert("That's right! bravo , I was born in Zarqa")
-        //console.log("My birth city is Zarqa")
-    } else if (myBirthCity === "no") {
-        alert("Sorry! that is a wrong answer ")
-        // console.log("Try again!")
+// 1st question:
+var correctAns = 0;
+while (myBirthCity !== "yes" && myBirthCity !== "no") {
+    var myBirthCity = prompt("Do you think I was born in Zarqa?")
+    myBirthCity = myBirthCity.toLowerCase();
+
+    console.log('Do you think I was born in Zarqa?');
+
+    switch (myBirthCity) {
+        case 'yes':
+            alert("That's right! bravo , I was born in Zarqa")
+            //console.log('correct answer')
+            correctAns = correctAns + 1;
+            break;
+
+        case 'no':
+            alert("Sorry! that is a wrong answer ")
+            // Console.log("Try again!")
+            break;
+    }
+}
+
+// 2nd question:
+
+while (favColor !== "yes" && favColor !== "no") {
+    var favColor = prompt("Do you think pink is Marah's favorate color?")
+    favColor = favColor.toLowerCase();
+
+    console.log("Do you think pink is Marah's favorate color?");
+
+    switch (favColor) {
+        case 'yes':
+            alert("That's right! bravo , my favorate color is pink")
+            //console.log('correct answer')
+            correctAns = correctAns + 1;
+            break;
+
+        case 'no':
+            alert("Sorry! that is a wrong answer ")
+            // Console.log("Try again!")
+            break;
+    }
+}
+
+
+// 3rd question:
+while (brothers !== "yes" && brothers !== "no") {
+    var brothers = prompt("Do i have brothers?")
+    brothers = brothers.toLowerCase();
+
+    console.log("Do i have brothers?");
+
+    switch (brothers) {
+        case 'no':
+            alert("That's right! bravo ,i have no brothers at all!")
+            //console.log('correct answer')
+            correctAns = correctAns + 1;
+            break;
+
+        case 'yes':
+            alert("Sorry! that is a wrong answer ")
+            // Console.log("Try again!")
+            break;
+    }
+}
+
+
+
+// fourth question:
+
+while (favMeal !== "yes" && favMeal !== "no") {
+    var favMeal = prompt("is Mansaf my favorate meal ?")
+    favMeal = favMeal.toLowerCase();
+
+    console.log("is Mansaf my favorate meal ?");
+
+    switch (favMeal) {
+        case 'no':
+            alert("That's right! bravo ,i don't like mansaf!")
+            //console.log('correct answer')
+            correctAns = correctAns + 1;
+            break;
+
+        case 'yes':
+            alert("Sorry! that is a wrong answer ")
+            // Console.log("Try again!")
+            break;
+    }
+}
+
+// fifth question :
+while (tall !== "yes" && tall !== "no") {
+    var tall = prompt("Am I 160 cm tall  ?")
+    tall = tall.toLowerCase();
+
+    console.log("Am I 160 cm tall  ?");
+
+    switch (tall) {
+        case 'no':
+            alert("That's right! bravo ,I'm 174 cm tall!")
+            //console.log('correct answer')
+            correctAns = correctAns + 1;
+            break;
+
+        case 'yes':
+            alert("Sorry! that is a wrong answer ")
+            // Console.log("Try again!")
+            break;
+    }
+}
+
+
+
+// this is lab03
+// 6th question:
+
+for (var counter = 0; counter <= 3; counter++) {
+    var number = prompt('How much time does Marah spend on practicing programming everyday? choose from 1-10 hours ')
+
+    console.log('How much time does Marah spend on practicing programming everyday? choose from 1-10 hours ')
+    if (number >= 1 && number <= 5) {
+
+        alert('Too low!');
+        console.log(' too low')
+    }
+    else if (number == 6) {
+
+        alert('correct!');
+        correctAns = correctAns + 1;
+        console.log('correct')
+        break;
+
+    }
+
+    else if (number > 6 && number <= 10) {
+        alert('too high!');
+        console.log('too high')
+
     }
     else {
-        alert("not acceptable answer")
-        // console.log("not true") }
-    }
-
-
-}
-
-var tall;
-while (tall !== "no") {
-    tall = prompt("how much tall do you think I am, 160? ");
-    tall.toLowerCase();
-
-    if (tall === "no") {
-        alert("That's right! bravo , I'm 174 cm tall");
-        //console.log("I'm 174 cm tall");
-    } else if (tall === "yes") {
-        alert("Sorry! that is a wrong answer ");
-        //console.log("Try again!");
-    }
-    else {
-        alert("not acceptable answer")
-        //console.log("not true")
-    }
-}
-
-var favMeal;
-while (favMeal !== "yes") {
-    favMeal = prompt("Is Mansaf my favorate meal??");
-    favMeal.toLowerCase();
-
-    if (favMeal === "yes") {
-        alert("That's right! bravo , it's Mansaf");
-        // console.log("My favorate meal is Mansaf");
-    } else if (favMeal === "no") {
-        alert("Sorry! that is a wrong answer ");
-        // console.log("Try again!");
-    } else {
-        alert("not acceptable answer")
-        //console.log("not true")
+        alert('out of range!')
+        console.log('out of range')
 
     }
-}
-
-var favColor;
-while (favColor !== "yes") {
-    favColor = prompt("is pink my favorate color?");
-    favColor.toLowerCase();
-
-    if (favColor === "yes") {
-        alert("That's right! bravo , its pink!");
-        //console.log("My favorite color is pink");
-    } else if (favColor === "no") {
-        alert("Sorry! that is a wrong answer ");
-        // console.log("Try again!")
-    } else {
-        alert("not acceptable answer")
-        //console.log("not true")
-    }
-
-}
-var brothers;
-while (brothers !== "no") {
-    brothers = prompt("do i have any brothers?");
-    brothers.toLowerCase();
-
-    if (brothers === "no") {
-        alert("That's right! bravo , I have no brothers at all");
-        //console.log("I have no brothers");
-    } else if (brothers === "yes") {
-        alert("Sorry! that is a wrong answer ");
-        //console.log("Try again!");
-    }
-    else {
-        alert("not acceptable answer")
-        //console.log("not true")
+    console.log('got here ' + counter)
+    if (counter === 3) {
+        alert('the correct answer is 6');
+        console.log('the correct answer is 6')
     }
 }
 
 
-alert("Thanks " + userName + " for answering questions about me! , now read a full bio in this page.");
+// 7th question:
+var userAnswer;
+var trueAnswer = ["black", "pink"];
+userAnswer = prompt('What colors Marah likes the most ?')
+console.log('What colors Marah likes the most ?')
+for (var counter = 0; counter < 5; counter++) {
+    if (userAnswer === trueAnswer[0] || userAnswer === trueAnswer[1]) {
+        alert('correct');
+        console.log("correct");
+        correctAns = correctAns + 1;
+        break;
+
+    }
+    else if (userAnswer !== trueAnswer[0] || userAnswer !== trueAnswer[1]) {
+        alert('wrong!')
+        console.log("wrong answer!")
+        userAnswer = prompt('What colors Marah likes the most ?')
+    }
+
+}
+alert('the correct answers are : pink and black!')
+alert('You got\b\b\b' + correctAns + '\b\b\banswers correct! Bravo.')
+
+
+alert('Thanks ' + userName + ' for answering questions about me! , now read a full bio in this page.');
 
 
 
